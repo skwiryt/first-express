5249e4c7d8b8c0e3d941d69128b547ae82df7aef
skwiryt <mzbprobny@gmail.com>
2021-06-15 14:32:26 +0200

    Add use of templates

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', {layout: 'dark'});
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.get('/hello/:name', (req, res) => {
+ res.render('hello', { name: req.params.name });
+ // res.send(`<h3>Hello ${req.params.name}</h3>`);
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-})
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
