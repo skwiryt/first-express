312c8c37ab0a6733c7d5869c732576f48c266713
skwiryt <mzbprobny@gmail.com>
2021-06-16 13:45:50 +0200

    Add contact form

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: 'public/uploads',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
 
+const upload = multer({ storage: storage })
+//const upload = multer()
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//app.use(multer().any()); - groźne!! i odradzane w dokumentacji;
+//app.use(multer().single('image'));
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -29,7 +43,19 @@ app.get('/history', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
  res.render('hello', { name: req.params.name });
- // res.send(`<h3>Hello ${req.params.name}</h3>`);
+});
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+  
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message && req.file) {
+    res.render('contact', {isSent: true, fileName: req.file.originalname});
+  }
+  else {
+    res.render('contact', {isError: true});
+  }
+
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
